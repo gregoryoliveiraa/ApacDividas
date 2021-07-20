07098cefe7991bf2d4d6198b9a786e4467238f01
Gregory Acacio Seibert Oliveira <gregory.oliveiraa@hotmail.com>
2021-07-20 14:04:05 -0300

    Delete conexao.php

--- conexao.php
+++
@@ -1,5 +0,0 @@
-<?php
-    $serverName = "10.105.111.2"; 
-    $connectionInfo = array( "Database"=>"APAC_DIVIDAS", "UID"=>"acesso.divida", "PWD"=>"*******");
-    $conn = sqlsrv_connect( $serverName, $connectionInfo);
-?>
